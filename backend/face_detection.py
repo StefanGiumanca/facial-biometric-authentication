6d26a82571d7f46cc69bbc95f5a88e649f14229f
StefanGiumanca <giumancastefan23@stud.ase.ro>
2026-02-09 18:10:25 +0200

    Haar Cascade face detection and test on images

--- backend/face_detection.py
+++ backend/face_detection.py
@@ -1,1 +1,38 @@
-#   Backend setup 
+import cv2
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
+img = cv2.imread("data/faces.png")
+
+if img is None:
+    print("Inexisting image.")
+    exit()
+
+# grayscale conversion
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#detect the faces
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(60,60)
+)
+
+# drawing the rectangle
+for (x,y,w,h) in faces:
+    cv2.rectangle(
+        img,
+        (x, y),
+        (x + w, y + h),
+        (0, 255, 0),
+        2
+    )
+
+#print the output
+cv2.imshow("Face Detection - Haar Cascades", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
